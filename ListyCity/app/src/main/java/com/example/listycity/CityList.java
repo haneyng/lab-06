5ba2a0d20358d85441aa3056e3125d155c889349
Han Nguyen <hnguyen5@ualberta.ca>
2025-10-17 11:19:38 -0600

    Add new methods for CityList class
    Add required methods from the exercise: hasCity(), deleteCity(), countCities()

--- ListyCity/app/src/main/java/com/example/listycity/CityList.java
+++ ListyCity/app/src/main/java/com/example/listycity/CityList.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 /**
  * This is a class that keeps a list of city objects.
- * @author CMPUT 301 - Lab 6 Tutorial
- * @version 1.0
+ * @author CMPUT 301 - Lab 6 Tutorial & Han Nguyen
+ * @version 1.1
  * @see City
  * @since ListyCity 1.0 (commit #1)
  */
@@ -28,12 +28,43 @@ public class CityList {
 
     /**
      * This method returns a sorted list of cities.
-     * @return - the sorted list
+     * @return the sorted list
      * @see <a href="https://howtodoinjava.com/java/collections/java-comparable-interface/">Java Comparable Interface</a>
      */
     public List<City> getCities() {
         List<City> list = cities;
         Collections.sort(list);
         return list;
+    }
+
+    /**
+     * This method checks if the given city is already on the list.
+     * @param city - the city to check
+     * @return true if the city is on the list, false if not
+     */
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
+
+    /**
+     * If the given city is already on the list, this method deletes the city.
+     * @param city - the city to delete
+     * @throws RuntimeException when trying to delete a city not on the list
+     */
+    public void deleteCity(City city) {
+        if (hasCity(city)) {
+            cities.remove(city);
+        }
+        else {
+            throw new RuntimeException("Cannot delete a city that doesn't exist.");
+        }
+    }
+
+    /**
+     * This method counts how many cities are there in the list.
+     * @return the amount of cities in the list
+     */
+    public int countCities() {
+        return cities.size();
     }
 }
